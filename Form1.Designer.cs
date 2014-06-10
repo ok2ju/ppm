b2608fc937ba83b3bb394b4ce3e8f27e893aa98d
Alexey Vakulich <soulSpringg@gmail.com>
2014-06-10 19:40:05 +0300

    repo init
    repo init

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace PPM
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBoxInput = new System.Windows.Forms.GroupBox();
+            this.ButtonInputGenerate = new System.Windows.Forms.Button();
+            this.GroupBoxTable = new System.Windows.Forms.GroupBox();
+            this.DataGirdView = new System.Windows.Forms.DataGridView();
+            this.Symbol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Context3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Context2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Context1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Chance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.GroupBoxInput.SuspendLayout();
+            this.GroupBoxTable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGirdView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GroupBoxInput
+            // 
+            this.GroupBoxInput.Controls.Add(this.richTextBox1);
+            this.GroupBoxInput.Controls.Add(this.ButtonInputGenerate);
+            this.GroupBoxInput.Location = new System.Drawing.Point(9, 270);
+            this.GroupBoxInput.Name = "GroupBoxInput";
+            this.GroupBoxInput.Size = new System.Drawing.Size(666, 152);
+            this.GroupBoxInput.TabIndex = 0;
+            this.GroupBoxInput.TabStop = false;
+            this.GroupBoxInput.Text = "Input Symbols";
+            this.GroupBoxInput.Enter += new System.EventHandler(this.GroupBoxInput_Enter);
+            // 
+            // ButtonInputGenerate
+            // 
+            this.ButtonInputGenerate.Location = new System.Drawing.Point(7, 121);
+            this.ButtonInputGenerate.Name = "ButtonInputGenerate";
+            this.ButtonInputGenerate.Size = new System.Drawing.Size(75, 23);
+            this.ButtonInputGenerate.TabIndex = 1;
+            this.ButtonInputGenerate.Text = "Generate";
+            this.ButtonInputGenerate.UseVisualStyleBackColor = true;
+            this.ButtonInputGenerate.Click += new System.EventHandler(this.ButtonInputGenerate_Click);
+            // 
+            // GroupBoxTable
+            // 
+            this.GroupBoxTable.Controls.Add(this.DataGirdView);
+            this.GroupBoxTable.Location = new System.Drawing.Point(12, 12);
+            this.GroupBoxTable.Name = "GroupBoxTable";
+            this.GroupBoxTable.Size = new System.Drawing.Size(666, 252);
+            this.GroupBoxTable.TabIndex = 1;
+            this.GroupBoxTable.TabStop = false;
+            this.GroupBoxTable.Text = "Frequence Table";
+            // 
+            // DataGirdView
+            // 
+            this.DataGirdView.AllowUserToAddRows = false;
+            this.DataGirdView.AllowUserToDeleteRows = false;
+            this.DataGirdView.AllowUserToOrderColumns = true;
+            this.DataGirdView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGirdView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Symbol,
+            this.Context3,
+            this.Context2,
+            this.Context1,
+            this.Chance});
+            this.DataGirdView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGirdView.Location = new System.Drawing.Point(3, 16);
+            this.DataGirdView.Name = "DataGirdView";
+            this.DataGirdView.ReadOnly = true;
+            this.DataGirdView.Size = new System.Drawing.Size(660, 233);
+            this.DataGirdView.TabIndex = 0;
+            // 
+            // Symbol
+            // 
+            this.Symbol.HeaderText = "Symbol";
+            this.Symbol.Name = "Symbol";
+            this.Symbol.ReadOnly = true;
+            // 
+            // Context3
+            // 
+            this.Context3.HeaderText = "Context 3";
+            this.Context3.Name = "Context3";
+            this.Context3.ReadOnly = true;
+            this.Context3.Width = 125;
+            // 
+            // Context2
+            // 
+            this.Context2.HeaderText = "Context 2";
+            this.Context2.Name = "Context2";
+            this.Context2.ReadOnly = true;
+            this.Context2.Width = 125;
+            // 
+            // Context1
+            // 
+            this.Context1.HeaderText = "Context 1";
+            this.Context1.Name = "Context1";
+            this.Context1.ReadOnly = true;
+            this.Context1.Width = 125;
+            // 
+            // Chance
+            // 
+            this.Chance.HeaderText = "Chance";
+            this.Chance.Name = "Chance";
+            this.Chance.ReadOnly = true;
+            this.Chance.Width = 125;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(647, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "bcbcabcbcabccbc";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 435);
+            this.Controls.Add(this.GroupBoxTable);
+            this.Controls.Add(this.GroupBoxInput);
+            this.Name = "MainWindow";
+            this.Text = "PPM Tables";
+            this.GroupBoxInput.ResumeLayout(false);
+            this.GroupBoxTable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGirdView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBoxInput;
+        private System.Windows.Forms.Button ButtonInputGenerate;
+        private System.Windows.Forms.GroupBox GroupBoxTable;
+        private System.Windows.Forms.DataGridView DataGirdView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Symbol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Context3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Context2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Context1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Chance;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PPM
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonInputGenerate_Click(object sender, EventArgs e)
+        {
+            //String StringOfSymbols = TextBoxInput.Text;
+            String StringOfSymbols = richTextBox1.Text;
+            if (StringOfSymbols.Length >= 4)
+            {
+                char[] SymbolsArray = StringOfSymbols.ToCharArray();
+                char[] SymbolsAlphabet = { 'a', 'b', 'c', 'd' };
+
+                int[] AbortSymbolOnLevel = { 0, 0, 0, 0 };
+
+                double[] SymbolChanceOnLevel3 = { 1, 1, 1, 1 };
+                double[] SymbolChanceOnLevel2 = { 1, 1, 1, 1 };
+                double[] SymbolChanceOnLevel1 = { 1, 1, 1, 1 };
+                double[] SymbolChance = { 1, 1, 1, 1 };
+
+                for (int CoddingSymbol = 0; CoddingSymbol < 4; CoddingSymbol++)
+                {                    
+                    bool AbortNext = false;
+                    int SymbolsContextFrequence = 0; //Частота появления символов в контексте
+                    int CoddingSymbolFrequence = 0;
+
+                    // Level = 3
+                    char[] Context = { SymbolsArray[StringOfSymbols.Length - 1], SymbolsArray[StringOfSymbols.Length - 2], SymbolsArray[StringOfSymbols.Length - 3] };
+                    for (int ContextCoddingSymbol = 0; ContextCoddingSymbol < 4; ContextCoddingSymbol++)
+                    {
+                        for (int N = 0; N < StringOfSymbols.Length - 3; N++)
+                        {
+                            if (Context[0] == SymbolsArray[N] && Context[1] == SymbolsArray[N + 1] && Context[2] == SymbolsArray[N + 2] && SymbolsAlphabet[ContextCoddingSymbol] == SymbolsArray[N + 3])
+                            {
+                                if(CoddingSymbol == ContextCoddingSymbol)
+                                {
+                                    CoddingSymbolFrequence++;
+                                    SymbolsContextFrequence++;
+                                    AbortSymbolOnLevel[CoddingSymbol] = 2;
+                                }
+                                else
+                                {
+                                    SymbolsContextFrequence++;
+                                }
+                            }
+                        }
+                    }
+                    if (CoddingSymbolFrequence > 0)
+                    {
+                        SymbolChanceOnLevel3[CoddingSymbol] *= (double)CoddingSymbolFrequence / (double)(SymbolsContextFrequence + 1);
+                        AbortNext = true;
+                    }
+                    else
+                    {
+                        SymbolChanceOnLevel3[CoddingSymbol] *= 1.0 / (SymbolsContextFrequence + 1);
+                    }
+
+                    // Level =  2
+                    if (AbortNext == false)
+                    {
+                        SymbolsContextFrequence = 0;
+                        CoddingSymbolFrequence = 0;
+                        Context[0] = SymbolsArray[StringOfSymbols.Length - 2];
+                        Context[1] = SymbolsArray[StringOfSymbols.Length - 1];
+                        for (int ContextCoddingSymbol = 0; ContextCoddingSymbol < 4; ContextCoddingSymbol++)
+                        {
+                            if (AbortSymbolOnLevel[ContextCoddingSymbol] < 2)
+                            {
+                                for (int N = 0; N < StringOfSymbols.Length - 2; N++)
+                                {
+                                    if (Context[0] == SymbolsArray[N] && Context[1] == SymbolsArray[N + 1] && SymbolsAlphabet[ContextCoddingSymbol] == SymbolsArray[N + 2])
+                                    {
+                                        if (CoddingSymbol == ContextCoddingSymbol)
+                                        {
+                                            CoddingSymbolFrequence++;
+                                            SymbolsContextFrequence++;
+                                            AbortSymbolOnLevel[CoddingSymbol] = 1;
+                                        }
+                                        else
+                                        {
+                                            SymbolsContextFrequence++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (CoddingSymbolFrequence > 0)
+                        {
+                            SymbolChanceOnLevel2[CoddingSymbol] *= (double)CoddingSymbolFrequence / (double)(SymbolsContextFrequence + 1);
+                            AbortNext = true;
+                        }
+                        else
+                        {
+                            SymbolChanceOnLevel2[CoddingSymbol] *= 1.0 / (SymbolsContextFrequence + 1);
+                        }
+                    }
+
+                    // Level = 1
+                    if (AbortNext == false)
+                    {
+                        SymbolsContextFrequence = 0;
+                        CoddingSymbolFrequence = 0;
+                        Context[0] = SymbolsArray[StringOfSymbols.Length - 1];
+                        for (int ContextCoddingSymbol = 0; ContextCoddingSymbol < 4; ContextCoddingSymbol++)
+                        {
+                            if (AbortSymbolOnLevel[ContextCoddingSymbol] < 1)
+                            {
+                                for (int N = 0; N < StringOfSymbols.Length - 1; N++)
+                                {
+                                    if (Context[0] == SymbolsArray[N] && SymbolsAlphabet[ContextCoddingSymbol] == SymbolsArray[N + 1])
+                                    {
+                                        if (CoddingSymbol == ContextCoddingSymbol)
+                                        {
+                                            CoddingSymbolFrequence++;
+                                            SymbolsContextFrequence++;
+                                            AbortSymbolOnLevel[CoddingSymbol] = 0;
+                                        }
+                                        else
+                                        {
+                                            SymbolsContextFrequence++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (CoddingSymbolFrequence > 0)
+                        {
+                            SymbolChanceOnLevel1[CoddingSymbol] *= (double)CoddingSymbolFrequence / (double)(SymbolsContextFrequence + 1);
+                            AbortNext = true;
+                        }
+                        else
+                        {
+                            SymbolChanceOnLevel1[CoddingSymbol] *= 1.0 / (SymbolsContextFrequence + 1);
+                        }
+                    }
+                }                
+
+                // Output
+                for (int N = 0; N < 4; N++)
+                {
+                    DataGirdView.Rows.Add(SymbolsAlphabet[N], SymbolChanceOnLevel3[N], SymbolChanceOnLevel2[N], SymbolChanceOnLevel1[N], SymbolChanceOnLevel1[N] * SymbolChanceOnLevel2[N] * SymbolChanceOnLevel3[N]);
+                }
+            }
+        }
+
+        private void GroupBoxInput_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
